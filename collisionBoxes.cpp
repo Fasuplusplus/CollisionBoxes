d4d05f92e3b45e57401a2ce52941bce29920acef
Fasuplusplus <facundo_forte@hotmail.com>
2021-05-29 22:22:56 -0300

    calculo de pi funcional

--- collisionBoxes.cpp
+++ collisionBoxes.cpp
@@ -4,11 +4,13 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
-double step = 0.0001;
+double decimals = 10; //para calcular pi
+double step = 2/(3.2*pow(10, decimals)*5);
+int collisions = 0;
 class body {
 public:
     double mass = 1;
-    double Xpos = 1000000000;
+    double Xpos = 10000000000;
     double Xvel = 0;
     double newXvel = 0;
     double radius = 0;
@@ -25,6 +27,9 @@ public:
     }
     void XellasticCollision(double otherMass, double otherXvel) {
         newXvel = (((Xvel * (mass - otherMass)) + (2 * otherMass * otherXvel)) / (mass + otherMass));
+    }
+    void XwallCollision() {
+        newXvel = -1 * Xvel;
     }
 };
 
@@ -47,11 +52,14 @@ void allCollisions() {
         for (int e = 0; e < boxcount; e++) {
             if (e != i) {
                 if (box[i].checkCollision(box[e].Xpos, box[e].radius) || box[i].checkCollision(wall.Xpos, wall.radius)) {
-                    printf("boing\n");
-                    printf("%i\n", i);
+                    collisions++;
                     box[i].XellasticCollision(box[e].mass, box[e].Xvel);
                 }
             }
+        }
+        if (box[i].checkCollision(wall.Xpos, wall.radius)) {
+            box[i].XwallCollision();
+            collisions++;
         }
     }
     for (int i = 0; i < boxcount; i++) {
@@ -62,21 +70,34 @@ void allCollisions() {
 void allMovements() {
     for (int i = 0; i < boxcount; i++) {
         box[i].move();
-        std::cout << "box " << i << " Xpos: " << box[i].Xpos << "  Xvel: " << box[i].Xvel << "\n";
     }
 }
 
 int main() {
-    placebody(box[0], 4, 2, 5, 1, true);
-    placebody(box[1], 9, 2, -5, 1, true);
+    placebody(wall, -2, 2, 0, 0, false);
+    placebody(box[0], 2.2, 2, 0, 1, true);
+    placebody(box[1], 6.4, 2, -5, pow(100, decimals), true);
     bool stop = false;
+    int check = 0;
     while (stop == false) {
         allCollisions();
         allMovements();
-        if (GetAsyncKeyState(VK_ESCAPE /*funciona no le hagas caso a las lineas rojas*/)) {
+        if (GetAsyncKeyState(VK_ESCAPE)) {
             printf("exited\n");
             stop = true;
         }
+        if ((box[0].Xvel >= 0) && (box[1].Xvel >= 0) && (box[0].Xvel < box[1].Xvel)) {
+            break;
+        }
+        check++;
+        if (check == 100000000) {
+            for (int i = 0; i < boxcount; i++) {
+                std::cout << "box " << i << " Xpos: " << box[i].Xpos << "  Xvel: " << box[i].Xvel << "\n";
+            }
+            std::cout << "pi: " << (collisions/2) << "\n";
+            check = 0;
+        }
     }
+    std::cout << "pi = " << (collisions / 2) << "\n";
     return 0;
 }
